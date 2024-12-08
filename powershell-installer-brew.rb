4788b626268a110e5b0d101755726142eb7e71e1
A01-opika <137298567+A01-opika@users.noreply.github.com>
2024-12-08 18:02:17 +0800

    Update powershell-installer-brew.rb

--- powershell-installer-brew.rb
+++ powershell-installer-brew.rb
@@ -3,7 +3,7 @@ class PowershellInstaller < Formula
   homepage "https://github.com/yourusername/homebrew-powershell-installer"
   url "https://raw.githubusercontent.com/A01-opika/Powershell_Installer/main/Powershell_Installer_bash.sh"
   version "1.0.0"
-  sha256 "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # 替换为脚本的SHA256
+  sha256 "9e9da55855df65aed882bcc4595ba4669583a523" # 替换为脚本的SHA256
 
   def install
     bin.install "powershell_installer.sh" => "powershell_installer"
